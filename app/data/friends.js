db44c8ec10448837453807fb6d4ba9080d99fc66
Erin0418 <erinf0418@gmail.com>
2018-07-23 18:24:22 -0400

    modal working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 {
   "name":"Mikey",
-  "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiqiJHe-LLcAhXPmeAKHcrzCY4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.obedding.com%2Fproducts%2Fmichelangelo-large-wall-accent-sticker-12pc-tmnt-ninja-turtle-self-stick-decals&psig=AOvVaw3cYdb9nZ3IiFzryFZfMLb4&ust=1532356978817748",
+  "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZjNPPoLbcAhXumeAKHZu1Dw8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.time2draw.com%2Fmanual%2Fhow-to-draw-a-teenage-mutant-ninja-turtles-step-by-step%2F&psig=AOvVaw3aSLFQ3gF-MQdj4jN9_GgQ&ust=1532470764803642",
   "scores":[
       5,
       1,
@@ -14,23 +14,7 @@ var friendsArray = [
       4,
       1
     ]
-},
-{
-    "name":"Donnie",
-    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiVnNKF-bLcAhVRm-AKHVM5AY4QjRx6BAgBEAU&url=https%3A%2F%2Fcomicvine.gamespot.com%2Fdonatello%2F4005-44709%2F&psig=AOvVaw0ot4MVxhUIVQcXksFS7pPH&ust=1532357003218841",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+}
 ]
 
 module.exports = friendsArray;

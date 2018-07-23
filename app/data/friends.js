23749c3bb553a8b5d2a5f9b9451bffbba4303973
Erin0418 <erinf0418@gmail.com>
2018-07-22 20:49:15 -0400

    working on questions

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+{
+  "name":"Mikey",
+  "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiqiJHe-LLcAhXPmeAKHcrzCY4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.obedding.com%2Fproducts%2Fmichelangelo-large-wall-accent-sticker-12pc-tmnt-ninja-turtle-self-stick-decals&psig=AOvVaw3cYdb9nZ3IiFzryFZfMLb4&ust=1532356978817748",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+    "name":"Donnie",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiVnNKF-bLcAhVRm-AKHVM5AY4QjRx6BAgBEAU&url=https%3A%2F%2Fcomicvine.gamespot.com%2Fdonatello%2F4005-44709%2F&psig=AOvVaw0ot4MVxhUIVQcXksFS7pPH&ust=1532357003218841",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+]
+
+module.exports = friendsArray;
